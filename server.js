a03b0ce373c3f6e75851a08d074a5d82854b1b0a
Sonali Rao <102312503+sonalirao14@users.noreply.github.com>
2024-06-22 12:23:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/v1/analytics",require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin",require("./routes/adminRoutes"));
 
 //static folders
-app.use(express.static(path.join(__dirname,'./client/build')))
+// app.use(express.static(path.join(__dirname,'./client/build')))
 //static routes
 // app.get("*",function(req,res){
 //     res.sendFile(path.join(__dirname,"./client/build/index.html"))
